e68cc1b6786b527801668e01559efab0d8b40de2
nguyenthanhtungn5 <nguyenthanhtungn5@gmail.com>
2019-01-09 23:45:38 +0100

    besser

--- person-scs/src/main/java/propra2/person/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package propra2.person;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
-
-public interface PersonRepository extends CrudRepository<Person,Long> {
-    List<Person> findAll();
-}
--- projekt-scs/src/main/java/propra2/projekt/ProjektRepository.java
+++
@@ -1,9 +0,0 @@
-package propra2.projekt;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
-
-public interface ProjektRepository extends CrudRepository<Projekt,Long> {
-    List<Projekt> findAll();
-}
